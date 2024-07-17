4c78ca8b91e09107593e845642fe0efa3cd26f01
hannan <sodrunberkicau@gmail.com>
2024-07-17 22:56:36 +0800

    add img

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,14 +9,6 @@ class HomeController extends Controller
 {
    function index() 
    {
-    $viewData = [];
-
-    $products = Product::get();
-    $products->each(function($data) {            
-        $data->image = "https://dummyimage.com/600x400/000/fff";
-    });
-
-    $viewData["products"] = $products;
      return view ('home.index');
    }
 }
